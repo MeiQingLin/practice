af92f1aa9f38e9e64cfa638399c5b4db8953cefb
梅清琳 <100503801+MeiQingLin@users.noreply.github.com>
2024-05-28 20:21:09 +0800

    Delete chapter03/src/oop03 directory

--- chapter03/src/oop03/SingletonTest.java
+++
@@ -1,9 +0,0 @@
-package oop03;
-
-public class SingletonTest {
-    public static void main(String[] args) {
-        Singleton s1 = Singleton.getInstance();
-        Singleton s2 = Singleton.getInstance();
-        System.out.println(s1 == s2);
-    }
-}
